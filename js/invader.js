ffa57ecb97df0218ce35ce7529eb5e00e26b4db5
s18001-ikeshiro <s18001@std.it-college.ac.jp>
2018-11-12 09:29:42 +0900

    Initial commit

---
+++ js/invader.js
@@ -0,0 +1,106 @@
+//グローバル展開
+phina.globalize();
+
+const SCREEN_WIDTH = 960;
+const SCREEN_HEIGHT = 640;
+const ASSETS = {
+    "image": {
+        "buro": "./assets/image/buropiyo.png",
+        "mero": "./assets/image/meropiyo.png",
+        "mika": "./assets/image/mikapiyo.png",
+        "nasu": "./assets/image/nasupiyo.png",
+        "take": "./assets/image/takepiyo.png",
+        "toma": "./assets/image/tomapiyo.png",
+    }
+};
+
+phina.define('MainScene', {
+    superClass: 'DisplayScene',
+    init: function () {
+        this.superInit({
+            width: SCREEN_WIDTH,
+            height: SCREEN_HEIGHT,
+        });
+        this.gridX = Grid(SCREEN_WIDTH, 40);
+        this.gridY = Grid(SCREEN_HEIGHT, 40);
+
+        this.backgroundColor = 'black';
+
+        const player = Player(
+            this.gridX.center(), this.gridY.span(37)).addChildTo(this);
+        )
+    }
+});
+
+phina.define("player", {
+    superClass: "Sprite",
+    init: function(x, y) {
+            this.superInit('toma', 64, 64);
+            this.setFrameIndex(10, 64, 64);
+            this.x = x;
+            this.y = y;
+            this.SPEED = 5;
+            this.bullet = null;
+    },
+
+    update:function (app) {
+        const key = app.keyboard;
+
+        if (key.getKey('left')) {
+            this.x -= this.SPEED;
+            if (this.left < 0) {
+                this.left = 0;
+            }
+        }
+        if (key.getKey('right')) {
+            this.x += this.SPEED;
+            if (this.right > SCREEN_WIDTH) {
+                this.right = SCREEN_WIDTH;
+            }
+        }
+
+        if (this.bullet == null && key.getKey('space')) {
+            this.bullet = Bullet(this.x, this.top).addChildTo(this.parent);
+        }
+
+        if (this.bullet != null && this.bullet.isInvalid) {
+            this.bullet.remove();
+            this.bullet = null;
+        }
+    }
+});
+
+phina.define('Bullet', {
+    superClass: 'RectangleShape',
+    init: function (x, y) {
+        this.superInit({
+            width: 3,
+            height: 15,
+            fill: 'white',
+            stroke: null,
+        });
+        this.x = x;
+        this.y = y;
+        this.invalid = false;
+        this.SPEED = 5;
+    },
+
+    update: function () {
+        this.y -= this.SPEED;
+        if (this.bottom < 0) {
+            this.isInvalid = true;
+        }
+    }
+});
+
+phina.main(() => {
+    const app = GameApp({
+        title: "インベーダー",
+        fps: 60,
+        width: SCREEN_WIDTH,
+        height: SCREEN_HEIGHT,
+        assets: ASEETS,
+    });
+
+    app.run();
+});
